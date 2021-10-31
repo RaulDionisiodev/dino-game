6181a96f66dea74e7f48b09bece1ea45130bf13e
Raul Dionisio <rauldionisiosh@gmail.com>
2021-10-31 13:04:46 -0300

    Criação do cactus e configuração do jogo

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
 let isJumping =  false;
+let position = 0;
 
 function handleKeyup(event){
     if(event.keyCode === 32){
@@ -11,7 +13,7 @@ function handleKeyup(event){
 }
 
 function jump(){
-    let position = 0;
+ 
 
     isJumping = true;
 
@@ -37,5 +39,33 @@ function jump(){
         }
     }, 20) 
 }
+
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randonTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px'
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if(cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            // game over
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">FIM DO JOGO </h1>';
+        }else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20)
+
+    setTimeout(createCactus, randonTime)
+}
+
+createCactus();
 
 document.addEventListener('keyup', handleKeyup)
